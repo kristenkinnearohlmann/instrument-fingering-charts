2c1f1ea83f8e9981c630c39ba73707e469035fd2
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-12-26 18:28:40 -0600

    Add PageHeader component

---
+++ src/components/PageHeader.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<div class="banner">
+	<p>
+		<strong>DRAFT CONTENT.</strong> Feedback welcome at
+		<a href="mailtp:kristenkinnearohlmann@gmail.com">kristenkinnearohlmann@gmail.com</a>
+	</p>
+</div>
+
+<style>
+	.banner {
+		background-color: pink;
+		border: 2px solid red;
+		text-align: center;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
 <script>
+	import PageHeader from '../components/PageHeader.svelte';
 	let name = 'world';
 </script>
 
+<svelte:head>
+	<title>Instrument Fingering Charts</title>
+</svelte:head>
+
+<PageHeader />
+
 <h1>Hello {name}!</h1>
+
+<style>
+</style>
