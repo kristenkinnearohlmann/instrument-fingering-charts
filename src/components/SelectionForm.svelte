a8ee2ddf2e61f023fbdd180c0711e875e4838872
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-12-29 21:07:19 -0600

    Add style for form component

--- src/components/SelectionForm.svelte
+++ src/components/SelectionForm.svelte
@@ -1,7 +1,7 @@
 <script></script>
 
 <main>
-	<div>
+	<div class="selection-form-component">
 		<p>Form will go here</p>
 	</div>
 </main>
@@ -10,5 +10,16 @@
 	main {
 		margin: 0 auto;
 		width: 95%;
+	}
+
+	.selection-form-component {
+		border: 2px solid gray;
+		border-radius: 8px;
+		display: flex;
+		flex-direction: column;
+		font-family: inherit;
+		font-size: inherit;
+		margin-bottom: 15px;
+		padding: 10px;
 	}
 </style>
